61d80af9257d916f7b99e1e7779021ff832009c0
Michael_Melnik <mikhamelnbr@yandex.ru>
2023-07-21 02:29:23 +0300

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\Yaml\Yaml;
+use Illuminate\Support\Facades\Hash;
 use App\Models\{TaskStatus, Task, Label, User};
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +18,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        User::factory(10)->create();
+        $users =  Yaml::parseFile(database_path('user.yml'));
+        foreach ($users as $user) {
+            User::firstOrCreate([
+                'name' => $user,
+                'email' => Str::random(10).'@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10)
+            ]);
+        }
 
         $statuses =  Yaml::parseFile(database_path('statuses.yml'));
         foreach ($statuses as $status) {
@@ -30,24 +42,24 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-//        $tasks = Yaml::parseFile(database_path('tasks.yml'));
-//        foreach ($tasks as $task) {
-//            Task::firstOrCreate([
-//                'name' => $task['name'],
-//                'description' => $task['description'],
-//                'status_id' => TaskStatus::inRandomOrder()->first()->id,
-//                'created_by_id' => User::inRandomOrder()->first()->id,
-//                'assigned_to_id' => User::inRandomOrder()->first()->id,
-//            ]);
-//        }
+        $tasks = Yaml::parseFile(database_path('tasks.yml'));
+        foreach ($tasks as $task) {
+            Task::firstOrCreate([
+                'name' => $task['name'],
+                'description' => $task['description'],
+                'status_id' => TaskStatus::inRandomOrder()->first()->id,
+                'created_by_id' => User::inRandomOrder()->first()->id,
+                'assigned_to_id' => User::inRandomOrder()->first()->id,
+            ]);
+        }
 //
-//        $labelsCount = Label::count();
-//        Task::all()->each(function ($task) use ($labelsCount) {
-//            $labels = Label::inRandomOrder()
-//                ->limit(rand(1, $labelsCount))
-//                ->get();
-//
-//            $task->labels()->attach($labels);
-//        });
+        $labelsCount = Label::count();
+        Task::all()->each(function ($task) use ($labelsCount) {
+            $labels = Label::inRandomOrder()
+                ->limit(rand(1, $labelsCount))
+                ->get();
+
+            $task->labels()->attach($labels);
+        });
     }
 }
