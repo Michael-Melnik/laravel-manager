5e7699953a6c49518c1eafd4833cb879f3302387
Michael_Melnik <mikhamelnbr@yandex.ru>
2023-07-20 23:33:48 +0300

    update

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,9 +3,9 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "Running npm"
-npm install
-npm run dev
+#echo "Running npm"
+#npm install
+#npm run dev
 
 echo "Caching config..."
 php artisan config:cache
